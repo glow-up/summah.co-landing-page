78dbb7171a336fd37da6eddbf2505771a2b46a0a
tashinga <mtokotashinga@gmail.com>
2021-04-02 22:33:33 +0200

    redirector page added

---
+++ js/redirector.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".carousel-container").forEach((carousel) => {
+    insertNumbers(carousel);
+
+    carousel.querySelector(".prev").addEventListener("click", (e) => {
+      minusItem(carousel);
+    });
+
+    carousel.querySelector(".next").addEventListener("click", () => {
+      plusItem(carousel);
+    });
+
+    //   insertDots(carousel);
+
+    //   carousel.querySelectorAll(".dot").forEach((dot) => {
+    //     dot.addEventListener("click", (e) => {
+    //       let item = Array.prototype.indexOf.call(
+    //         e.target.parentNode.children,
+    //         e.target
+    //       );
+
+    //       showItems(carousel, item);
+    //     });
+    //   });
+
+    showItems(carousel, 0);
+  });
+});
+
+function insertNumbers(carousel) {
+  const length = carousel.querySelectorAll(".item").length;
+  for (let i = 0; i < length; i++) {
+    const nmbr = document.createElement("div");
+    nmbr.classList.add("numbertext");
+    nmbr.innerText = i + 1 + " / " + length;
+
+    carousel.querySelectorAll(".item")[i].append(nmbr);
+  }
+}
+
+function plusItem(carousel) {
+  let item = currentItem(carousel);
+
+  carousel
+    .querySelectorAll(".item")
+  [item].nextElementSibling.classList.contains("item")
+    ? showItems(carousel, item + 1)
+    : showItems(carousel, 0);
+}
+
+function minusItem(carousel) {
+  let item = currentItem(carousel);
+
+  carousel.querySelectorAll(".item")[item].previousElementSibling != null
+    ? showItems(carousel, item - 1)
+    : showItems(carousel, carousel.querySelectorAll(".item").length - 1);
+}
+
+function currentItem(carousel) {
+  return [...carousel.querySelectorAll(".item")].findIndex(
+    (item) => item.style.display == "block"
+  );
+}
+
+function showItems(carousel, item) {
+  if (carousel.querySelectorAll(".item")[currentItem(carousel)] != undefined)
+    carousel.querySelectorAll(".item")[currentItem(carousel)].style.display =
+      "none";
+  carousel.querySelectorAll(".item")[item].style.display = "block";
+}
+
+
+//AUTO MOVE CAROUSEL
+var slideMoveTime = 5000;
+setInterval(function () {
+  plusItem(document.querySelector(".carousel-container"))
+}, slideMoveTime);
+
+
+function openLink (theLink) {
+  window.parent.location.href= theLink;
+}
